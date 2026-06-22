273159ea9014fd5869f60aecb84bc153287feee4
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-22 14:21:15 +0500

    Update __init__.py

--- cleaneasy/__init__.py
+++ cleaneasy/__init__.py
@@ -1,16 +1,29 @@
+from __future__ import annotations
+
 from .core import CleanEasy
-from .utils import get_column_types, setup_logger, convert_to_dataframe
-from .validators import check_missing_proportion, check_normality, check_unique_values, check_skewness, check_correlation
+from .utils import (
+    convert_to_dataframe,
+    get_column_types,
+    setup_logger,
+)
+from .validators import (
+    check_correlation,
+    check_missing_proportion,
+    check_normality,
+    check_skewness,
+    check_unique_values,
+)
 
 __version__ = "0.2.0"
-__all__ = [
-    'CleanEasy',
-    'get_column_types',
-    'setup_logger',
-    'convert_to_dataframe',
-    'check_missing_proportion',
-    'check_normality',
-    'check_unique_values',
-    'check_skewness',
-    'check_correlation'
-]
+
+__all__ = (
+    "CleanEasy",
+    "get_column_types",
+    "setup_logger",
+    "convert_to_dataframe",
+    "check_missing_proportion",
+    "check_normality",
+    "check_unique_values",
+    "check_skewness",
+    "check_correlation",
+)
